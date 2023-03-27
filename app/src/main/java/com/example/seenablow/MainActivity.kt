e16746e3cf4264e6e17769920f070d7bce166f3d
WrappingorNot <WrappingorNot@github.com>
2023-03-27 13:13:04 +0900

    first

--- app/src/main/java/com/example/seenablow/MainActivity.kt
+++ app/src/main/java/com/example/seenablow/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.seenablow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.seenablow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+    }
+
+
+    // 액티비티가 파괴될 때..
+    override fun onDestroy() {
+        // onDestroy 에서 binding class 인스턴스 참조를 정리해주어야 한다.
+        binding = null
+        super.onDestroy()
     }
 }
